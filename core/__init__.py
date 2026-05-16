9e30d2dbf6fabfe3415c0ac482816f205bb5ab09
vxnkx <271046360+vxnkx@users.noreply.github.com>
2026-05-16 02:50:24 +0200

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+#  _   _                 _ 
+# | \ | |               (_)
+# |  \| | __ ___   __ _  _ 
+# | . ` |/ _` \ \ / /(_)| |
+# | |\  | (_| |\ V /  _ | |
+# |_| \_|\__,_| \_/  (_)|_|
+# 
+# Navi Multitool - Developed by x
+# GitHub: https://github.com/vxnkx/navi-multitool
+
+# core package
